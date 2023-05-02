76ce666fa8984cedd7ac01487f4e00d49c49e723
luigi <luigilamastra@gmail.com>
2023-05-02 22:33:14 +0200

    added timestamp for server

--- cron.js
+++ cron.js
@@ -1,11 +1,16 @@
 import cron from 'node-cron'
 // import { generateAllImages } from './cards/generateAllImages.js'
 import { cronGetHoroscopesAndSaveToDB } from './cronHoroscopes.js'
+import { getItalianTimestamp } from './helpers/dates.js'
 
 console.log('cron started')
 
 
 cron.schedule('00 19 * * *', ()=> cronGetHoroscopesAndSaveToDB(3),  {
+    scheduled: true,
+    timezone: "Europe/Rome"
+  })
+cron.schedule('*/1 * * * *', ()=> console.log(getItalianTimestamp()),  {
     scheduled: true,
     timezone: "Europe/Rome"
   })
