f5eb235514734180efb05863b71273da83d1912b
luigi <luigilamastra@gmail.com>
2023-05-12 01:40:43 +0200

    added log

--- cron.js
+++ cron.js
@@ -3,15 +3,19 @@ import cron from 'node-cron'
 import { cronGetHoroscopesAndSaveToDB } from './cronHoroscopes.js'
 import { getItalianTimestamp } from './helpers/dates.js'
 import { tweetAllImages, tweetTest } from './twitter/tweetImage.js'
-
+const timestamp = getItalianTimestamp()
 console.log('cron started')
 
 
-cron.schedule('35 1 * * *', ()=> cronGetHoroscopesAndSaveToDB(2),  {
+cron.schedule('00 5 * * *', ()=> cronGetHoroscopesAndSaveToDB(3),  {
     scheduled: true,
     timezone: "Europe/Rome"
   })
 cron.schedule('30 7 * * *', ()=> tweetAllImages(0),  {
+    scheduled: true,
+    timezone: "Europe/Rome"
+  })
+cron.schedule('*/10 * * * *', ()=> console.log(timestamp),  {
     scheduled: true,
     timezone: "Europe/Rome"
   })
