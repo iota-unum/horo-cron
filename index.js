09fda5af0bdfc9bc33058897f606affd69616fa9
luigi <luigilamastra@gmail.com>
2023-05-18 02:20:16 +0200

    modified schedule

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config();
 async function main() {
   try {
     const day = 2
-    // await generateAllImages(-7)
+    await generateAllImages(1)
 // await tweetAllImages(0)
 //     const day = 3 // 0 === today, 1 === tomorrow , -1 yesterday
 //     const {dateColons, dateDashes} = getFormattedDate(0)
@@ -29,7 +29,7 @@ async function main() {
 // }
 // console.log('SAVED ALL!!!')
 // await tweetTest(0)
-   await  cronGetHoroscopesAndSaveToDB(1)
+  //  await  cronGetHoroscopesAndSaveToDB(1)
   //   //generate daily horoscope
   //   // const sign = 'toro'
   //   const { dateColons, dateDashes } = getFormattedDate(3);
