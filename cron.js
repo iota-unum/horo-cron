09323aa09325bb8828426fa054a921c2528bf872
luigi <luigilamastra@gmail.com>
2023-05-22 10:45:54 +0200

    timestamp

--- cron.js
+++ cron.js
@@ -1,36 +1,35 @@
-import cron from 'node-cron'
+import cron from "node-cron";
 // import { generateAllImages } from './cards/generateAllImages.js'
-import { cronGetHoroscopesAndSaveToDB } from './cronHoroscopes.js'
-import { getItalianTimestamp } from './helpers/dates.js'
-import { tweetAllImages, tweetTest } from './twitter/tweetImage.js'
-const timestamp = getItalianTimestamp()
-console.log('cron started')
+import { cronGetHoroscopesAndSaveToDB } from "./cronHoroscopes.js";
+import { getItalianTimestamp } from "./helpers/dates.js";
+import { tweetAllImages, tweetTest } from "./twitter/tweetImage.js";
+console.log("cron started");
 
+cron.schedule("37 10 * * *", () => cronGetHoroscopesAndSaveToDB(3), {
+  scheduled: true,
+  timezone: "Europe/Rome",
+});
+cron.schedule("30 7 * * *", () => tweetAllImages(0), {
+  scheduled: true,
+  timezone: "Europe/Rome",
+});
+cron.schedule("40 19 * * *", () => tweetAllImages(1), {
+  scheduled: true,
+  timezone: "Europe/Rome",
+});
+cron.schedule(
+  "*/30 * * * *",
+  () => {
+    const timestamp = getItalianTimestamp();
 
-cron.schedule('37 10 * * *', ()=> cronGetHoroscopesAndSaveToDB(3),  {
+    console.log(timestamp);
+  },
+  {
     scheduled: true,
-    timezone: "Europe/Rome"
-  })
-cron.schedule('30 7 * * *', ()=> tweetAllImages(0),  {
-    scheduled: true,
-    timezone: "Europe/Rome"
-  })
-cron.schedule('40 19 * * *', ()=> tweetAllImages(1),  {
-    scheduled: true,
-    timezone: "Europe/Rome"
-  })
-cron.schedule('*/10 * * * *', ()=> console.log(timestamp),  {
-    scheduled: true,
-    timezone: "Europe/Rome"
-  })
+    timezone: "Europe/Rome",
+  }
+);
 // cron.schedule('*/5 * * * *', ()=> tweetTest(0),  {
 //     scheduled: true,
 //     timezone: "Europe/Rome"
 //   })
-
-
-
-
-
-
-
